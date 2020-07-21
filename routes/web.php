3cc1993e189e1db110ec66656f22ac2885ca8636
kinoshita0623 <kuutarousii@gmail.com>
2020-07-21 16:54:01 +0900

    Welcomeページを変更した

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@files')->name('home');
 
 Route::get('/files', 'HomeController@files')->name('files');
 
